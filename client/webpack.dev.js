de9d9677c20da45af93f1e634e2fb031a353120f
Shoji Makino <pitang1965@gmail.com>
2021-05-15 21:30:34 +0900

    Fix typo

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -2,11 +2,10 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  mode: 'development',
   output: {
     publicPath: `http://localhost:8080/`,
   },
-
-  mode: 'development',
   devServer: {
     port: 8080,
   },
--- server/server.js
+++ server/server.js
@@ -53,11 +53,21 @@ const resolvers = {
   },
 };
 
+const startTime = Date.now();
+console.log('#1', Date.now() - startTime);
+
 const pubsub = new PubSub();
+
+console.log('#2', Date.now() - startTime);
+
 const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
+
+console.log('#3', Date.now() - startTime);
 
 const port = process.env.PORT || 4000;
 
 server.start(({ port }) => {
   console.log(`Server is up on port ${port}/`);
+
+  console.log('#4', Date.now() - startTime);
 });
